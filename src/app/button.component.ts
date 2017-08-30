879d56ca06eee2151030426c2270af9d4b398e16
Flayre <bgsasing18@yahoo.com>
2017-08-30 10:06:49 +0800

    commit

--- src/app/button.component.ts
+++ src/app/button.component.ts
@@ -1,10 +1,6 @@
+import {Output, EventEmitter } from '@angular/core';
 
 
-<div id="options">
-  <button id="addStudents" (click)="addStudentEntry()">
-    Add Student Entry
-  </button>
-  <button id="listStudents" (click)="listStudents()">
-    List Students
-  </button>
-</div>
+export class ButtonComponent {
+
+}
